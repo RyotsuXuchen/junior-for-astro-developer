7df04c61a142f1f1e5cbb8a1d1dccfe052b5fc61
Ryotsu Kyokushin <wuxuchennb@gmail.com>
2026-07-10 05:20:56 +0800

    继承preact, 添加Preact欢迎横幅

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,15 @@ import { defineConfig } from "astro/config";
 
 import tailwindcss from "@tailwindcss/vite";
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://offical-astro-tutorial-project.netlify.app",
+
   vite: {
     plugins: [tailwindcss()],
   },
+
+  integrations: [preact()],
 });
